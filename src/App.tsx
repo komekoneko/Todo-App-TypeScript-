0a381aaf01f7f20fb393439eae2f63c0687cf673
komekoneko <kai.s20050701@gmail.com>
2026-07-07 18:15:28 +0900

    add todo function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react'
 
+
+type Todo = {
+  id: number,
+  text: string,
+  done: boolean
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [ todos, setTodos ] = useState<Todo[]>([])
+  const [ input, setInput] = useState("");
+
+  const addTodo = () => {
+    const result = [...todos, { id: Date.now(), text: input, done: false }];
+    setTodos(result);
+    setInput("");
+  }
 
   return (
     <>
       <h1>Todo App</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={addTodo}>追加</button>
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.text}</li>
+        })}
+      </ul>
     </>
   )
 }
